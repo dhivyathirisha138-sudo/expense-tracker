745bd9a5750a1e646e02741cd470f9ad464332a0
dhivyathirisha138-sudo <dhivyathirisha138@gmail.com>
2026-04-06 20:19:58 +0530

    v3 total feature working

--- app.py
+++ app.py
@@ -2,32 +2,35 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Store expenses in a list
 expenses = []
 
-# Home route
+# Home
 @app.route('/')
 def home():
-    return "Expense Tracker v2 Running"
+    return "Expense Tracker v3 Running"
 
-# 1️⃣ Add Expense
+# Add Expense
 @app.route('/add', methods=['POST'])
 def add_expense():
     data = request.get_json()
-    
     expense = {
         "name": data.get("name"),
         "amount": data.get("amount")
     }
-    
     expenses.append(expense)
-    
-    return jsonify({"message": "Expense added successfully", "data": expense})
+    return jsonify({"message": "Added", "data": expense})
 
-# 2️⃣ View Expenses
+# View Expenses
 @app.route('/view', methods=['GET'])
 def view_expenses():
     return jsonify(expenses)
+
+# ✅ NEW FEATURE (v3)
+# Total Expense
+@app.route('/total', methods=['GET'])
+def total_expense():
+    total = sum(item["amount"] for item in expenses)
+    return jsonify({"total": total})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
